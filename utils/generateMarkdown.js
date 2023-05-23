0f1642708a7803714a5c520c9ffd8b2711dc3b2c
mserafin20 <mmserafin20@gmail.com>
2023-05-23 14:24:14 -0400

    finished markdown file and tested functionality, added results folder

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -55,11 +55,62 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.title} ${renderLicenseBadge(data.license)}
+
+  ## Description
+
+  ${data.description}
+
+  
+  ## Table of Contents
+
+  [Installation](#installation)
+
+  [Usage](#usage)
+
+  [License](#license)
+
+  [Contribution](#contribution)
+
+  [Test](#test)
+
+  [Questions](#questions)
+
+
+  ## Installation 
+
+  ${data.installation}
+
+
+  ## Usage
+
+  ${data.usage}
+
+
+  ## License 
+
+  ${data.license}
+
+
+  ## Contribution
+
+  ${data.contribution}
+
+  ## Test
+
+  ${data.test}
+
+  ## Questions
+
+  ### View below for additional information about this project, including a Github account and Email address for contact:
+
+  [Github](https://github.com/${data.githubUserName})
+
+  [Email](mailto: ${data.email})
 
 `;
 }
