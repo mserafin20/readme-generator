a403f7ce5bd2429c6c4f8cab494d30e2f6b4d809
mserafin20 <mmserafin20@gmail.com>
2023-05-23 09:55:28 -0400

    Installed inquirer version 8.2.4

--- utils/index.js
+++ utils/index.js
@@ -1,4 +1,7 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+
+console.log(inquirer);
 
 // TODO: Create an array of questions for user input
 const questions = [];
